2f97df818d723a4ae1054b1fba51c871822f398e
John <jlawrencesha@gmail.com>
2021-03-29 10:32:51 -0700

    new changes

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -15,3 +15,11 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+data class SleepNight(
+
+        var nightId: Long = 0L,
+        val startTimeMilli: Long = System.currentTimeMillis(),
+        var endTimeMilli: Long = startTimeMilli,
+        var sleepQuality: Int = -1
+)
